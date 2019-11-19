f7bf4de387f73506c79009edd254540d3a2f08f5
Mark Rivers <rivers@cars.uchicago.edu>
2019-11-19 10:13:10 -0600

    Remove asyn dependency for timing, use epicsTime

--- newportApp/src/testSFTPUpload.cpp
+++ newportApp/src/testSFTPUpload.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include <asynDriver.h>
+#include <epicsTime.h>
 #include <xpsSFTPUpload.h>
 
 int main(int argc, char *argv[])
 {
   bool sftpVerbose = true;
-  asynUser *pasynUser = pasynManager->createAsynUser(0, 0);
-  asynPrint(pasynUser, ASYN_TRACE_ERROR, "Calling xpsSFTPUpload\n");
+  epicsTimeStamp startTime, endTime;
+  epicsTimeGetCurrent(&startTime);
+  printf("Calling xpsSFTPUpload\n");
   int status = xpsSFTPUpload("164.54.160.71", "/Admin/Public/Trajectories", "TrajectoryScan.trj", 
                              "Administrator", "Administrator", sftpVerbose);
-  asynPrint(pasynUser, ASYN_TRACE_ERROR, "xpsSFTPUpload returned status=%d\n", status);
+  epicsTimeGetCurrent(&endTime);
+  double elapsedTime = epicsTimeDiffInSeconds(&endTime, &startTime);
+  printf("xpsSFTPUpload returned status=%d, elapsed time=%f\n", status, elapsedTime);
   return 0;
 }
